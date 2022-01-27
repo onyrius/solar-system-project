76c9613be0d045be68c268b30e37dd4306e30802
Suelen Arruda <suelenonyrius@gmail.com>
2022-01-28 00:11:16 +0100

    requisito 5

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Title from './Title';
+// import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
@@ -7,6 +9,7 @@ class SolarSystem extends React.Component {
       <>
         <div data-testid="solar-system" />
         <Title headline="Planetas" />
+        <PlanetCard />
       </>);
   }
 }
