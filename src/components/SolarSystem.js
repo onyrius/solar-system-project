953d41ba6b5f65b37ec5b5cc56d7adf5aa230ad8
Suelen Arruda <suelenonyrius@gmail.com>
2022-01-28 10:56:27 +0100

    conserta lint

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -11,13 +11,11 @@ class SolarSystem extends React.Component {
       <>
         <div data-testid="solar-system" />
         <Title headline="Planetas" />
-        {Planets.map(({ name, image }) => { // desestrutura o obj de data/planets e faz map para listar todos os planetas.
-        return (<PlanetCard
+        {Planets.map(({ name, image }) => (<PlanetCard
           key={ name }
           planetName={ name }
           planetImage={ image }
-        />);
-       })}
+        />))}
       </>);
   }
 }
